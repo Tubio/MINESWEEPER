3c2b31547da7e23f80e0dce34095dc9d228c62a3
Tubio <32169593+Tubio@users.noreply.github.com>
2017-12-12 18:04:52 -0300

    Update juez.h

--- ULTIMO/juez.h
+++ ULTIMO/juez.h
@@ -62,23 +62,56 @@ class Juez{
 		* 	en el tablero.
 		* Post: da su turno al jugadorActual y muestra el tablero de juego.*/
 		void sigueJugando(Jugador* jugadorActual,Mapa* tableroDeJuego);
-
+		
+		 /* pre: 
+		 * post: crea un archivo de texto con los puntajes de los jugadores.
+		 */
 		void crearArchivoConPuntajes();
-
+		
+		/* pre: la lista de jugadores ya ha sido creada
+		* post: calcula con los puntajes quien fue el jugador que mas puntaje tuvo he imprime por 
+		* pantalla la respectiva felicitacion segun hubo ganador o un empate.
+		*/
 		void encontrarJugadorQueGanoPorPuntaje();
-
+		
+		/*pre:
+		*post: devuelve la cantidad de banderas(marcas) que se ubicaron correctamente, es decir donde 
+		* efectivamente habia una mina.
+		*/
 		uint buscarBanderasCorrectas();
-
+		
+		/*pre: recibe por parametro una bandera y una lista de minas ya inicializada. 
+		* post: devuelve si la bandera actual coincide con una mina, es decir estan en la misma ubicacion.
+		*/
 		bool banderaEsCorrecta(Bandera actual,Lista<Mina>* minas);
 
 		uint calcularJugadoresQuePerdieron();
 
+		/*pre: se llama cuando el jugador quiere realizar la jugada de deshacer o rehacer.
+		* post: mientras que el jugador diga que quiere seguir haciendo cambios y se le permita
+		* va a pedir al usuario que opcion elige y realiza el rehacer o deshacer jugada respectivamente.
+		*/
 		void realizarCambios();
 
+		/*pre: se llama cuando el jugador a pedido deshacer una jugada
+		* post: segun sea la ultima jugada reaizada se ubica al jugador que la hizo y se lama al antidestapador
+		* o al marcador para deshacer la jugada hecha por el jugador y modificar su puntaje, y en caso de que haya perdido
+		* el jugador que hizo la jugada, volverlo a poner en juego; todo esto mientras que hayan jugadas por deshacer, 
+		* retorna si es posible o no seguir deshaciendo.
+		*/
 		bool deshacerJugada();
-
+		
+		/*pre: se llama cuando el jugador a pedido la opcion de rehacer una jugada
+		*post: le muestra al usuario por pantalla las opciones de jugadas que se pueden rehacer y segun la opcion 
+		* que dio el usuario se llama al rehacer paralela con esa opcion; todo esto mientras que se puedan rehacer jugadas,
+		* retorna si se puede seguir rehaciendo jugadas.
+		*/
 		bool rehacerJugada();
-
+		
+		/*pre: se llama desde el rehacer jugada y recibe por parametro la posicion de la jugada que el jugador quiere rehacer 
+		* post: encuentra la jugada que esta en la posicion indicada, rehace la jugada y deja la jugada actual del grafo 
+		* apuntando a la jugada que se rehizo.
+		*/
 		void rehacerParalela(uint posicion);
 };
 
